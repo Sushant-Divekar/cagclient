913d4f72bfb78e17f57a62839e3fdc849707ed73
Sushant-Divekar <116884359+Sushant-Divekar@users.noreply.github.com>
2024-08-27 20:39:40 +0530

    Update MtoP.js

--- src/components/MtoP.js
+++ src/components/MtoP.js
@@ -161,7 +161,7 @@ const MtoP = () => {
             Shift: shift,
           };
           const response = await fetch(
-            "http://127.0.0.1:5000/predictPercentile",
+            "https://cagserver.onrender.com/predictPercentile",
             {
               method: "POST",
               headers: {
